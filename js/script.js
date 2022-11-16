7b4d828a01f0cb70909d220b78647ea2f00af6f0
marrcosmunoz <munoz.marcos.epet20@gmail.com>
2022-11-16 20:17:32 -0300

    Solucion problemas css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const div = document.getElementById('div');
 
 function movimiento(){
-    const input = document.getElementById('input').value;
+    const input = document.getElementById('password').value;
     const h1 = document.createElement('h1');
     console.log(input);
     h1.textContent = input;
