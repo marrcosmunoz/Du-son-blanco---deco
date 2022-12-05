be7d53ae9e226215ec14f46f99489ab292ff7208
marrcosmunoz <munoz.marcos.epet20@gmail.com>
2022-12-05 11:48:45 -0300

    Nosotros html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-const div = document.getElementById('div');
+const name = document.getElementById('nombre');
+const password = document.getElementById('password');
+const sumbit = document.getElementById('sumbit');
+const form = document.getElementById('form');
+const user = document.getElementById('user');
+const parrafo = document.getElementById('warning');
 
-function movimiento(){
-    const input = document.getElementById('password').value;
-    const h1 = document.createElement('h1');
-    console.log(input);
-    h1.textContent = input;
-    div.appendChild(h1);
-    div.classList.add('rotar');
-}
+
+form.addEventListener('sumbit',(m)=>{
+    m.preventDefault()
+    let warning =""
+    let entrar = false
+    let regexUser = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    if(nombre.Value.length <6){
+        warning += "El nombre no es valido <br>"
+        entrar = true
+    }
+    if(!regexUser.test(email.Value)){
+        warning += "El email no es valido <br>"
+        entrar = true
+    }
+    if(password.Value.length >8){
+        warning += "Contraseña incorrecta <br>"
+        entrar = true
+    }
+    if (entrar){
+        parrafo.innerHTML = warning
+    }
+})
